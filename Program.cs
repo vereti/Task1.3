4f96e77ebd2b2403f7db323a6294f61793a93d10
vereti <peace.atomka@yandex.ru>
2017-07-06 20:33:35 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n: ");
+            int[] M = new int[10];
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a;
+            int i;
+
+
+            while (n > 0)
+            {
+                n = Math.DivRem(n, 10, out a);
+                M[a]++;
+            }
+            for (i = 0; i < M.Length; i++)
+                Console.WriteLine("{0}: {1}", i, M[i]);
+            Console.ReadKey();
+        }
+    }
+}
